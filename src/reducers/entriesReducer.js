0273e13e7c385f8d81dcf06d744cb4fa7a08a2a4
Chaitanya karthik basva <chaitanyakarthikbasva@gmail.com>
2025-11-16 11:29:02 +0530

    react redux store setup done

---
+++ src/reducers/entriesReducer.js
@@ -0,0 +1,52 @@
+const initialEntries = [
+    {
+      id: 1,
+      description: "This is my First entry",
+      isExpense: true,
+      value: 1234,
+    },
+    {
+      id: 2,
+      description: "This is my Second entry",
+      isExpense: true,
+      value: 665,
+    },
+    {
+      id: 3,
+      description: "This is my Third entry",
+      isExpense: false,
+      value: 872.9,
+    },
+    {
+      id: 4,
+      description: "This is my Fourth entry",
+      isExpense: false,
+      value: 982,
+    },
+    {
+      id: 5,
+      description: "This is my Sixth entry",
+      isExpense: true,
+      value: 237.8,
+    },
+    {
+      id: 6,
+      description: "This is my Seventh entry",
+      isExpense: false,
+      value: 789.63,
+    },
+  ];
+  
+export const entriesReducer = (state = initialEntries, action) => {
+  let newEntries;
+  switch (action.type) {
+    case "ADD_ENTRY":
+      newEntries = [...state, { ...action.payload }];
+      return newEntries;
+    case "REMOVE_ENTRY":
+      newEntries = state.filter((el) => el.id !== action.payload.id);
+      return newEntries;
+    default:
+      return state;
+  }
+};
